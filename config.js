08bcbee3743de1fd835d750965298585fdc08612
Ɗᴀʀᴋ Ɗєνιʟ 👑🇵🇰🖤 <umarofficial404@gmail.com>
2024-12-02 13:38:47 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+OWNER_NUMBER: '923165123719@s.whatsapp.net',
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/IfaDdJf.jpeg",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi UD-MD Is Online Now 💻\n*💻 Owner* - UMAR REHMAN\n\n*💻 Owner Number* +923165123719\n\n_Type .menu to get the command menu._",
+READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE: "true",
+ELEVENLABS_API_KEY: 'sk_4669df1ac80b92a39528954725c4ff916fbfc7e3a4d75b22',
+};
